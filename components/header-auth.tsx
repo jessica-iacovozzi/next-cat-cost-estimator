e23c7513644388d2bf133107e0141dc8d7d8ee80
Jessica Iacovozzi <iacovozzi.jessica@gmail.com>
2025-02-19 09:50:42 -0500

    Added dashboard link to logged in users

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -52,6 +52,9 @@ export default async function AuthButton() {
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
+      <Button asChild size="sm" variant={"link"}>
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
       <form action={signOutAction}>
         <Button type="submit" variant={"outline"}>
           Sign out
