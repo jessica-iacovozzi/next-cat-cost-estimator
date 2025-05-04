b5e6dd7007d2df6cb02d74f31aefe6b9e6f51899
Jessica Iacovozzi <iacovozzi.jessica@gmail.com>
2025-05-04 11:45:14 -0400

    Change main tag to div in home page component

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-8">
+    <div className="flex flex-col items-center justify-center p-8">
       <h1 className="text-3xl text-center font-bold">🐱 Thinking of getting a cat in Quebec?</h1>
       <p className="mt-2 mb-6 text-center">Estimate your yearly costs in under 1 minute.</p>
       <Link href="/estimate">
         <Button aria-label="Start Estimating" variant="destructive" onClick={() => {localStorage.removeItem("catCostBreakdown")}}>Start Estimating</Button>
       </Link>
-    </main>
+    </div>
   );
 }
